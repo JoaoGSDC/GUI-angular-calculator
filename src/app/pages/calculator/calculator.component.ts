6a4cc7ede20195879e098fcb6a0dca5b893a783f
JoaoGSDC <joaogsdc@gmail.com>
2020-09-15 16:51:45 -0300

    Project #2

--- src/app/pages/calculator/calculator.component.ts
+++ src/app/pages/calculator/calculator.component.ts
@@ -1,69 +1,125 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 enum Operations {
-  SumOperation = "SumOperation",
-  SubtractOperation = "SubtractOperation",
-  MultiplyOperation = "MultiplyOperation",
-  DivisionOperation = "DivisionOperation",
+  SumOperation = 'SumOperation',
+  SubtractOperation = 'SubtractOperation',
+  MultiplyOperation = 'MultiplyOperation',
+  DivisionOperation = 'DivisionOperation',
 }
 
 @Component({
-  selector: "app-calculator",
-  templateUrl: "./calculator.component.html",
-  styleUrls: ["./calculator.component.scss"],
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.scss'],
 })
 export class CalculatorComponent implements OnInit {
   op = Operations;
 
-  resultValueInput: string = "";
+  equalsClick: boolean = false;
   blockActions: boolean = false;
+
+  firstNumber: string = '';
+  secondNumber: string = '';
+  operationValue: string = '';
+  resultValueInput: string = '';
 
   constructor() {}
 
   ngOnInit(): void {}
 
   operationCalculator(value: string): void {
-    const mathOperations = {
-      SumOperation() {
-        return "+";
-      },
-
-      SubtractOperation() {
-        return "-";
-      },
-
-      MultiplyOperation() {
-        return "x";
-      },
-
-      DivisionOperation() {
-        return "/";
-      },
-    };
-
-    if (!isNaN(Number(value))) {
-      this.resultValueInput += value;
-      // this.blockActions = false;
+    if (this.equalsClick) {
+      this.resetValues();
+      this.equalsClick = false;
     }
 
-    /* if (this.blockActions) {
-      return;
-    } */
+    const mathOperations = this.commandsMathOperation();
+
+    if (!isNaN(Number(value)) || value === '.') {
+      if (!this.blockActions) {
+        this.firstNumber += value;
+      } else {
+        this.secondNumber += value;
+      }
+
+      this.resultValueInput += value;
+    }
 
     const mathOperation = mathOperations[value];
     if (!!mathOperation && !this.blockActions) {
+      this.operationValue = value;
       this.resultValueInput += mathOperation();
       this.blockActions = true;
     }
-
-    /* if (value === ".") {
-      this.resultValueInput += ".";
-      this.blockActions = true;
-    } */
   }
 
   calculate(): void {
-    const total = 1 + 2;
+    const firstNumber: number = Number(this.firstNumber);
+    const secondNumber: number = Number(this.secondNumber);
+    const calculateTotal: any = this.calculateTotal();
+    const total: number | string = calculateTotal[this.operationValue](
+      firstNumber,
+      secondNumber
+    );
+
+    this.equalsClick = true;
     this.resultValueInput = String(total);
+  }
+
+  resetValues(): void {
+    this.blockActions = false;
+    this.firstNumber = '';
+    this.secondNumber = '';
+    this.operationValue = '';
+    this.resultValueInput = '';
+  }
+
+  calculateTotal(): any {
+    const calculateOperations = {
+      SumOperation(firstNumber: number, secondNumber: number): number {
+        return firstNumber + secondNumber;
+      },
+
+      SubtractOperation(firstNumber: number, secondNumber: number): number {
+        return firstNumber - secondNumber;
+      },
+
+      MultiplyOperation(firstNumber: number, secondNumber: number): number {
+        return firstNumber * secondNumber;
+      },
+
+      DivisionOperation(
+        firstNumber: number,
+        secondNumber: number
+      ): number | string {
+        return secondNumber !== 0
+          ? firstNumber / secondNumber
+          : 'Operação inválida!';
+      },
+    };
+
+    return calculateOperations;
+  }
+
+  commandsMathOperation(): any {
+    const mathOperations = {
+      SumOperation() {
+        return '+';
+      },
+
+      SubtractOperation() {
+        return '-';
+      },
+
+      MultiplyOperation() {
+        return 'x';
+      },
+
+      DivisionOperation() {
+        return '/';
+      },
+    };
+
+    return mathOperations;
   }
 }
